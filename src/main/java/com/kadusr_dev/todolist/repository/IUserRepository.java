ccfe1710f3c6e395d14583b13b056fed3c0d17f0
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-03 17:00:17 -0300

    feat(user): Add unique username constraint
    - Added unique constraint to username field.
    - Updated UserModel and IUserRepository.

--- src/main/java/com/kadusr_dev/todolist/repository/IUserRepository.java
+++ src/main/java/com/kadusr_dev/todolist/repository/IUserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kadusr_dev.todolist.model.UserModel;
 
+
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
 
 }
