3c7b62fdb533afb954010d06f3ba7e95cf8959b4
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-03 16:08:15 -0300

    `Refactor UserController and UserModel: update POST mapping to "/novo" and add getter and setter methods for UserModel fields.`

--- src/main/java/com/kadusr_dev/todolist/user/UserModel.java
+++ src/main/java/com/kadusr_dev/todolist/user/UserModel.java
@@ -2,8 +2,35 @@ package com.kadusr_dev.todolist.user;
 
 public class UserModel {
 
-    String name;
+    private String nome;
     private String username;
     private String password;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+
 
 }
