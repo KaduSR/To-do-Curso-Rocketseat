835a998dd9e8766139b047d9c2be531028dd094f
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-03 16:41:22 -0300

    `Added H2 database dependency and configuration for Spring Boot application, including changes to pom.xml, UserModel.java, and application.properties.`

--- src/main/java/com/kadusr_dev/todolist/user/UserModel.java
+++ src/main/java/com/kadusr_dev/todolist/user/UserModel.java
@@ -1,12 +1,30 @@
 package com.kadusr_dev.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_user")
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String nome;
+
     private String username;
+
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
 
 }
