80aeae3b05d8500a845d8f6920ddd8a070a74dea
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-03 16:13:01 -0300

    `Added import statement and annotation to UserModel.java`

--- src/main/java/com/kadusr_dev/todolist/user/UserModel.java
+++ src/main/java/com/kadusr_dev/todolist/user/UserModel.java
@@ -1,12 +1,12 @@
 package com.kadusr_dev.todolist.user;
 
-@
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String nome;
     private String username;
     private String password;
-
-
 
 }
