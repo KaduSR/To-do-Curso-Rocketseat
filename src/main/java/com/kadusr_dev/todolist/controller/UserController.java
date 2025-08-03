42e4bd55d8658011bf717bf406df2a9885f4975e
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-03 17:05:22 -0300

    feat(user): improve user creation endpoint
    - Return ResponseEntity instead of UserModel
    - Handle existing username with BAD_REQUEST
    - Return CREATED status on success
    - Improved response message

--- src/main/java/com/kadusr_dev/todolist/controller/UserController.java
+++ src/main/java/com/kadusr_dev/todolist/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.kadusr_dev.todolist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +19,17 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/novo")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        
         var user = this.userRepository.findByUsername(userModel.getUsername());
+
         if (user != null) {
             System.out.println("Usúario já existe");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usúario já existe");
         }
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated + " criado com sucesso");
     }
 
 }
