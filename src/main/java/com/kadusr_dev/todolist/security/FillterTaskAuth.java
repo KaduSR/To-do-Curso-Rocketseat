18c32915aeac5400c887b590f078ddd2eb8dd61f
Kadu Ribeiro <kaduesr@gmail.com>
2025-08-03 18:31:33 -0300

    feat(security): Implement Basic Authentication
    - Added Basic Authentication filter.
    - Decodes Base64 encoded credentials.
    - Extracts username and password.
    - Prints credentials to console (for testing).

--- src/main/java/com/kadusr_dev/todolist/security/FillterTaskAuth.java
+++ src/main/java/com/kadusr_dev/todolist/security/FillterTaskAuth.java
@@ -1,6 +1,7 @@
 package com.kadusr_dev.todolist.security;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,8 +17,25 @@ public class FillterTaskAuth extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        request.getHeader("Athorization");
-                
+        var authorization = request.getHeader("Authorization");
+
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecode);
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+
+
+
+
         filterChain.doFilter(request, response);
 
     }
